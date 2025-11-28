3b1dd09334228e477130be62760ca60feefe2e04
Arif Bepari <168634392+Arif2146@users.noreply.github.com>
2025-11-28 12:17:59 +0530

    today

--- arrays/Solution.java
+++ arrays/Solution.java
@@ -48,3 +48,50 @@ Given a string sentence containing only lowercase English letters, return true i
         return charSet.size() == 26;
     }
 }*/
+/*Given an array nums of integers, return how many of them contain an even number of digits. */
+/*class Solution {
+    public int findNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            int number = nums[i];
+            while (number != 0) {
+                number /= 10;
+                count++;
+            }
+            if (count % 2 == 0) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 0;
+            }
+        }
+        int evenCount = 0;
+        for (int num : nums) {
+            evenCount += num;
+        }
+        return evenCount;
+    }
+}*/
+/*The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices. */
+/*class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposed = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+}*/
+/*The array-form of an integer num is an array representing its digits in left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k. */
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        
+    }
+}
