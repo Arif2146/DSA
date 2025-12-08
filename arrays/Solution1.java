caa70282098f516e02a5c29c138904e8c677cf4c
Arif Bepari <168634392+Arif2146@users.noreply.github.com>
2025-12-08 13:36:10 +0530

    Update Solution1.java
    Given a unsorted array arr[] of positive integers having all the numbers occurring exactly twice, except for one number which will occur only once. Find the number occurring only once

--- arrays/Solution1.java
+++ arrays/Solution1.java
@@ -98,3 +98,19 @@ public static void main(String[] args)
     }
 }
  */
+//Given a unsorted array arr[] of positive integers having all the numbers occurring exactly twice, except for one number which will occur only once. Find the number occurring only once.
+/* class Solution1 {
+    public int findUnique(int[] arr) {
+        int res=0;
+        for(int i=0;i<arr.length;i++){
+            res=res^arr[i];
+        }
+        return res; 
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1, 2, 3, 5, 3, 2, 1};
+        Solution1 s=new Solution1();
+        System.out.println(s.findUnique(arr));
+    }
+} */
