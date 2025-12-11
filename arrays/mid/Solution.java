c11140e72563898336496d09dc05406fb0a529f4
Arif Bepari <168634392+Arif2146@users.noreply.github.com>
2025-12-11 13:16:09 +0530

    Increasing Triplet Subsequence
    https://leetcode.com/problems/increasing-triplet-subsequence

--- arrays/mid/Solution.java
+++ arrays/mid/Solution.java
@@ -53,3 +53,27 @@ class Solution {
     }
 }
  */
+//334. Increasing Triplet Subsequence
+//https://leetcode.com/problems/increasing-triplet-subsequence
+/* class Solution {
+    public boolean increasingTriplet(int[] nums) {
+    int first = Integer.MAX_VALUE;
+    int second = Integer.MAX_VALUE;
+    for (int n : nums) {
+        if (n <= first) {
+            first = n;
+        } else if (n <= second) {
+            second = n;
+        } else {
+            return true; // means first < second < n
+        }
+    }
+
+    return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = { 1,2,3,4,5};
+        Solution s=new Solution();
+        System.out.println(s.increasingTriplet(arr));
+    }
+} */
