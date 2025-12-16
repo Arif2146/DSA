77db6ef75f49eb43f1547a1cc49ca9373e88ea50
Arif Bepari <168634392+Arif2146@users.noreply.github.com>
2025-12-16 14:24:15 +0530

    3Sum Closest
    https://leetcode.com/problems/3sum-closest

--- arrays/mid/Solution.java
+++ arrays/mid/Solution.java
@@ -176,5 +176,43 @@ class Solution {
         return false;
     }
 } */
+//3Sum Closest
+//https://leetcode.com/problems/3sum-closest
+/* import java.util.Arrays;
+class Solution {
+     public static void main(String[] args) {
+        int arr[] = {-1,2,1,-4};
+        int target = 1;
+        Solution s = new Solution();
+        System.out.println(s.threeSumClosest(arr, target));
+    }
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int closestSum = nums[0] + nums[1] + nums[2];
 
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
 
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+                if (currentSum < target) {
+                    left++;
+                } else if (currentSum > target) {
+                    right--;
+                } else {
+                    return currentSum; // Exact match found
+                }
+            }
+        }
+
+        return closestSum;
+    }
+}
+ */
