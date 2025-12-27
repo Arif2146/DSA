58a14e3f203bcc924b7421d4c0740ccbcb66fde9
Arif Bepari <arifbepari2006@gmail.com>
2025-12-27 14:06:21 +0530

    binary search

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -21,7 +21,7 @@
     }
 } */
 
-import java.util.ArrayList;
+/* import java.util.ArrayList;
 
 class Solution1 {
     static ArrayList<Integer> leaders(int arr[]) {
@@ -45,4 +45,30 @@ class Solution1 {
         int[] arr = {16, 17, 4, 3, 5, 2};
         System.out.println(leaders(arr)); // Output: 1
     }
-}
+} */
+
+//704. Binary Search
+//https://leetcode.com/problems/binary-search
+/* class Solution1 {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+           int mid = start + (end - start) / 2;
+           if(nums[mid]>target){
+                end = mid - 1;
+              } else if(nums[mid]<target){
+                start = mid + 1;
+              } else {
+                return mid;
+           }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(sol.search(nums, target)); // Output: 4
+    }
+} */
