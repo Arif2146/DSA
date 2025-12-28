1ac660136554e511586b949a90dcb52dd611134a
Arif Bepari <arifbepari2006@gmail.com>
2025-12-28 13:38:12 +0530

    189. Rotate Array

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -72,3 +72,30 @@ class Solution1 {
         System.out.println(sol.search(nums, target)); // Output: 4
     }
 } */
+//189. Rotate Array
+//https://leetcode.com/problems/rotate-array
+class Solution1 {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // In case k is greater than n
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+     void reverse(int[] nums, int start, int end) {
+            while (start < end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        sol.rotate(nums, k);
+        System.out.println("Rotated array: " + java.util.Arrays.toString(nums)); // Output: [5,6,7,1,2,3,4]
+    }
+}
