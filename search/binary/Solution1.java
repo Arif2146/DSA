e22305659239493f62df3be6cb995a41dc22306e
Arif Bepari <arifbepari2006@gmail.com>
2025-12-31 12:48:09 +0530

    33. Search in Rotated Sorted Array

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -149,8 +149,6 @@ class Solution1 {
             }
         }
         return start;
-
-        
     }
     public static void main(String[] args) {
         Solution1 sol = new Solution1();
@@ -161,3 +159,38 @@ class Solution1 {
 } */
 //33. Search in Rotated Sorted Array
 //https://leetcode.com/problems/search-in-rotated-sorted-array
+/* class Solution1 {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            // Left half is sorted
+            if(nums[start] <= nums[mid]) {
+                if(target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } 
+            // Right half is sorted
+            else {
+                if(target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(sol.search(arr, target));
+    }
+} */
