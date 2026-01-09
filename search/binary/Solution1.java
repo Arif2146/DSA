4dc92442ecef79ba2712f816fbf187d6f68d381b
Arif Bepari <arifbepari2006@gmail.com>
2026-01-09 12:28:01 +0530

    162. Find Peak Element

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -267,3 +267,25 @@ class Solution1 {
         System.out.println(sol.singleNonDuplicate(arr));
     }
 } */
+//162. Find Peak Element
+//https://leetcode.com/problems/find-peak-element
+/* class Solution1 {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+     public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int[] arr = {1,2,1,3,5,6,4};
+        System.out.println(sol.findPeakElement(arr));
+    }
+} */
