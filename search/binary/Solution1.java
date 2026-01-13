0870d676e7d574ee43be4d15a281d39ce5acce53
Arif Bepari <arifbepari2006@gmail.com>
2026-01-13 14:53:07 +0530

    167. Two Sum II - Input Array Is Sorted

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -348,3 +348,29 @@ class Solution1 {
         System.out.println(sol.nthRoot(4,16));
     }
 } */
+//167. Two Sum II - Input Array Is Sorted
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+/* class Solution1 {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+        while(start < end) {
+           int sum = numbers[start] + numbers[end];
+           if (sum == target){
+            return new int[]{start + 1, end + 1};
+           } else if (sum < target) {
+            start++;
+           } else {
+            end--;
+           }
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] result = sol.twoSum(nums, target);
+        System.out.println("Result: [" + result[0] + "," + result[1] + "]");
+    }
+} */
