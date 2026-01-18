a420a2dbd1317b870c68faea29996654b4a36a66
Arif Bepari <arifbepari2006@gmail.com>
2026-01-18 14:03:02 +0530

    441. Arranging Coins

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -401,3 +401,28 @@ class Solution1 {
     }
 }
  */
+//441. Arranging Coins
+//https://leetcode.com/problems/arranging-coins
+/* class Solution1 {
+    public int arrangeCoins(int n) {
+        int start = 0;
+        int end = n;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            long coins = (long)mid * (mid + 1) / 2;
+            if(coins == n) {
+                return mid;
+            } else if(coins < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end; 
+    }
+    public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int n = 5;
+        System.out.println(sol.arrangeCoins(n));
+    }
+} */
