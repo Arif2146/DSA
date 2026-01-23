eb938614dac36c1babcb05f8a41a6f736e546766
Arif Bepari <arifbepari2006@gmail.com>
2026-01-23 11:21:56 +0530

    35. Search Insert Position

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -451,3 +451,28 @@ class Solution1 {
     }
 
 } */
+//35. Search Insert Position
+//https://leetcode.com/problems/search-insert-position
+/* class Solution1 {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+   public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int[] nums = {1,3,5,6};
+        int target = 2;
+        System.out.println(sol.searchInsert(nums, target)); // Output: 2
+    }
+} */
