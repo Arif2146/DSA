a887bc49da7c1f7077736c450668c37dd4ccfb21
Arif Bepari <arifbepari2006@gmail.com>
2026-01-29 16:27:19 +0530

    754. Reach a Number

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -594,3 +594,22 @@ class Solution1 {
         System.out.println(sol.checkIfExist(arr));
     }
 } */
+//754. Reach a Number
+//https://leetcode.com/problems/reach-a-number
+/* class Solution1 {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int sum = 0;
+        int steps = 0;
+        while (sum < target || (sum - target) % 2 != 0) {
+            steps++;
+            sum += steps;
+        }
+        return steps;
+    }
+    public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int target = 2;
+        System.out.println(sol.reachNumber(target));
+    }
+} */
