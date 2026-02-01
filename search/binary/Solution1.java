dc90b523ed2b861ed5fc31bd836659ffd2218cc3
Arif Bepari <arifbepari2006@gmail.com>
2026-02-01 14:26:57 +0530

    practice rotated sorted binary

--- search/binary/Solution1.java
+++ search/binary/Solution1.java
@@ -613,3 +613,40 @@ class Solution1 {
         System.out.println(sol.reachNumber(target));
     }
 } */
+// practice algoritms
+/* class Solution1 {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            // Left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (nums[start]<= target && target < nums[mid] ) {
+                    end = mid -1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            else{
+                if (nums[end] >= target && target > nums[mid]) {
+                    start = mid + 1;
+                }
+                else{
+                    end = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Solution1 sol = new Solution1();
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 6;
+        System.out.println(sol.search(nums, target)); // Output: 4
+    }
+} */
