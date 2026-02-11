266de98e7e7c0f42dd5c762e8689dea2b1512f0b
Arif Bepari <arifbepari2006@gmail.com>
2026-02-11 12:56:43 +0530

    41. First Missing Positive

--- sorting/Solution.java
+++ sorting/Solution.java
@@ -169,3 +169,43 @@ class Solution {
 
 }
  */
+
+//41. First Missing Positive
+//https://leetcode.com/problems/first-missing-positive
+
+/* class Solution {
+    public static void main(String[] args) {
+        int[] arr = { 3, 4, -1, 1 };
+        Solution s = new Solution();
+        System.out.println(s.firstMissingPositive(arr));
+    }
+
+    public int firstMissingPositive(int[] nums) {
+        sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+} */
