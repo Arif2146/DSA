fe26e1db506188dfb46e776a0d7e3bd896c7be65
Arif Bepari <arifbepari2006@gmail.com>
2026-03-31 21:43:48 +0530

    28. Find the Index of the First Occurrence in a String

--- string/Solution.java
+++ string/Solution.java
@@ -1,3 +1,23 @@
+//28. Find the Index of the First Occurrence in a String
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
 class Solution {
-    
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.strStr("sadbutsad", "sad")); // Output: 0
+        System.out.println(solution.strStr("leetcode", "leeto")); // Output: -1
+
+    }
 }
