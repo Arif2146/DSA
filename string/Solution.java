09e73c80a1b53aa2b8c5c8f01045b6f028cd5db4
Arif Bepari <arifbepari2006@gmail.com>
2026-04-01 11:37:35 +0530

    344. Reverse String

--- string/Solution.java
+++ string/Solution.java
@@ -1,6 +1,6 @@
 //28. Find the Index of the First Occurrence in a String
 //https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
-class Solution {
+/* class Solution {
     public int strStr(String haystack, String needle) {
         if (needle.isEmpty()) {
             return 0;
@@ -21,3 +21,26 @@ class Solution {
 
     }
 }
+ */
+//344. Reverse String
+//https://leetcode.com/problems/reverse-string
+/* class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        solution.reverseString(s);
+        System.out.println(new String(s)); // Output: "olleh"
+
+    }
+} */
